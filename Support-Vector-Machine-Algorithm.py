0066c8eb26a7dbf551b37b62159fd138bf2ea8db
Onur Yücedağ <olmaditekrar@gmail.com>
2017-10-09 16:08:53 +0300

    Training Algorithm

--- Support-Vector-Machine-Algorithm.py
+++ Support-Vector-Machine-Algorithm.py
@@ -10,8 +10,43 @@ class Support_Vector_Machine:
 		if self.visualization:
 			self.fig = plt.figure()
 			self.ax = self.fig.add_subplot(1,1,1)
+	# train 
 	def fit(self,data):
-		pass
+		self.data = data
+		#{||W||: [w,b] }
+		opt_dict = {}
+		transforms = 	[[1,1],
+						[-1,1],
+						[-1,-1],
+						[1,-1]]
+		#For get the maximum and minimum ranges both for graph and starting value for stepping .
+		all_data = []
+		for yi in self.data :
+			for featureset in self.data[yi]:
+				for feature in featureset:
+					all_data.append(feature)
+		self.max_feature_value = max(all_data)
+		self.min_feature_value = min(all_data)
+		all_data = None
+
+
+		step_sizes = [self.max_feature_value * 0.1,
+						self.max_feature_value * 0.01,
+						#Point of expense :
+						self.max_feature_value * 0.001]
+		#Extremely expensive
+		b_range_multiple = 5
+		b_multiple = 5
+
+		latest_optimum = self.max_feature_value*10
+		for step in step_sizes:
+			w = np.array([latest_optimum,latest_optimum])
+			# We can do this because it is convex 
+			optimized = False
+			while not optimized :
+				pass
+
+
 	def predict(self,features):
 		# sign(x.w+b)
 		classification = np.sign(np.dot(np.array(features),self.w)+self.b)
